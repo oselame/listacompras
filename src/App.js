06ef8a1004f6032fb8368a64edbb6f55e8442b35
Adriano Oselame <adrianooselame@gmail.com>
2019-01-28 20:59:55 -0200

    Header da aplicação

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { createMuiTheme } from '@material-ui/core/styles';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import AppBar from '@material-ui/core/AppBar';
+
 
 import './App.css';
 
@@ -19,6 +21,11 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
+        <AppBar position="static" color="primary">
+          <div className="header">
+            <p className="title">Lista de Compras</p>
+          </div>
+        </AppBar>
         <div>
             <h1>Teste</h1>
         </div>
