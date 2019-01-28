09a71486de499c603c315926e7d2af649ab88d73
Adriano Oselame <adrianooselame@gmail.com>
2019-01-28 21:29:32 -0200

    Organizando o projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { createMuiTheme } from '@material-ui/core/styles';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import AppBar from '@material-ui/core/AppBar';
-
 
 import './App.css';
+
+import Header from './common/Header';
+import Home from './home';
 
 const theme = createMuiTheme({
   palette: {
@@ -21,14 +22,8 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <AppBar position="static" color="primary">
-          <div className="header">
-            <p className="title">Lista de Compras</p>
-          </div>
-        </AppBar>
-        <div>
-            <h1>Teste</h1>
-        </div>
+        <Header />
+        <Home />
       </MuiThemeProvider>
     );
   }
---
+++ src/common/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+
+const Header = () => (
+    <AppBar position="static" color="primary">
+        <div className="header">
+        <p className="title">Lista de Compras</p>
+        </div>
+    </AppBar>
+)
+
+export default Header;
