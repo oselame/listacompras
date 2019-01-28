481331ccb4d8d0741071e9ab27f00ade6c35582d
Adriano Oselame <adrianooselame@gmail.com>
2019-01-28 20:46:59 -0200

    Criando temas com material-ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+
 import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#e91e63',
+    },
+    secondary: {
+      main: '#00b0ff',
+    }
+  },
+});
 
 class App extends Component {
   render() {
     return (
-      <div>
-          <h1>Teste</h1>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div>
+            <h1>Teste</h1>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
