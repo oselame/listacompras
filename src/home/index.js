71d68b92f3e611755e9576461e42eb788bce0924
Adriano Oselame <adrianooselame@gmail.com>
2019-01-28 21:58:01 -0200

    Iniciando a Home Page

--- src/home/index.js
+++ src/home/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+
+import './Home.css';
 
 const Home = (props) => (
-    <div>
-        <h1>Home Page</h1>
+    <div className="page-container">
+        
+        <div className="new-list-container">
+            <Card className="card">
+                <CardActionArea className="card-action-area">
+                    <CardContent className="card-content">
+                        <div>
+                            <p className="title">Adicionar Novas Listas!</p>
+                        </div>
+
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </div>
+
+
     </div>
 )
 
