eb27eaaadf22c8e423ec47b3e81473c1faaf4fbb
Adriano Oselame <adrianooselame@gmail.com>
2019-01-29 21:22:50 -0200

    Refatorando a Home Page

--- src/home/index.js
+++ src/home/index.js
@@ -1,59 +1,45 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Divider from '@material-ui/core/Divider';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingBasket, faCheck } from '@fortawesome/free-solid-svg-icons';
+
+import CustomCard from '../common/CustomCard';
 
 import './Home.css';
 
 const Home = (props) => (
     <div className="page-container">
         
-        <div className="new-list-container">
-            <Card className="card">
-                <CardActionArea className="card-action-area">
-                    <CardContent className="card-content">
-                        <div>
-                            <p className="title">Adicionar Novas Listas!</p>
-                        </div>
+        <CustomCard containerClass="new-list-container">
+            <div>
+                <p className="title">Adicionar Novas Listas!</p>
+            </div>
+        </CustomCard>
 
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        </div>
 
-        <div className="list-container">
-            <Card className="card">
-                <CardActionArea className="card-action-area">
-                    <CardContent className="card-content">
-                        <div>
-                            <p className="title">Mês</p>
-                            <div className="list-card-body">
-                                <div className="list-card-item">
-                                    <FontAwesomeIcon icon={faShoppingBasket} size="sm" />
-                                    <p>1 Item(s) Restante</p>
-                                </div>
-                                <div className="list-card-item">
-                                <FontAwesomeIcon icon={faCheck} size="sm" />
-                                    <p>2 Item(s) Comprados</p>
-                                </div>
-                            </div>
-                        </div>
-                    </CardContent>
-                </CardActionArea>
-                
-                <Divider />
-                <CardActions className="card-footer">
+        <CustomCard 
+            containerClass="list-container"
+            footer={
+                <div className="list-footer">
                     <p>01/01/2019</p>
                     <p>R$ 1000,00</p>
-                </CardActions>
-
-            </Card>
-        </div>
+                </div>
+            }
+            >
+            <div>
+                <p className="title">Mês</p>
+                <div className="list-card-body">
+                    <div className="list-card-item">
+                        <FontAwesomeIcon icon={faShoppingBasket} size="sm" />
+                        <p>1 Item(s) Restante</p>
+                    </div>
+                    <div className="list-card-item">
+                    <FontAwesomeIcon icon={faCheck} size="sm" />
+                        <p>2 Item(s) Comprados</p>
+                    </div>
+                </div>
+            </div>
+        </CustomCard>
 
     </div>
 )
