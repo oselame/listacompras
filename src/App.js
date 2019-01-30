d4970f74c66bd8db6305910718119a06787187b6
Adriano Oselame <adrianooselame@gmail.com>
2019-01-29 22:29:01 -0200

    Construção de Rotas

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import { createMuiTheme } from '@material-ui/core/styles';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 import Header from './common/Header';
 import Home from './home';
+import CreateList from './createList';
 
 const theme = createMuiTheme({
   typography: {
@@ -25,8 +28,15 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <Header />
-        <Home />
+        <Router>
+          <div>
+            <Header />  
+            <Switch>
+              <Route exact path='/'      component={ Home } />
+              <Route       path='/lista' component={ CreateList } />
+            </Switch>
+          </div>
+        </Router>
       </MuiThemeProvider>
     );
   }
