7cb0660e1437a917f1e33afb3715cc8c9dc6cecf
Adriano Oselame <adrianooselame@gmail.com>
2019-01-31 21:59:30 -0200

    Criando o form e adiconando TextField

--- src/createList/index.js
+++ src/createList/index.js
@@ -1,7 +1,62 @@
 import React from 'react';
 
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import { InputAdornment } from '@material-ui/core';
+
+const unitys = ['kg', 'lt', 'un'];
+
 const CreateList = () => (
-    <h1>Create List</h1>
+    <div className="page-container">
+
+        <form className="form-container">
+            <TextField
+                label="Lista"
+                name="list"
+                value={''}
+                onChange={ () => {} }
+                required
+            />
+            <TextField
+                label="Produto"
+                name="product"
+                value={''}
+                onChange={ () => {} }
+                required
+            />
+            <TextField
+                label="Quantidade"
+                name="quantity"
+                value={''}
+                onChange={ () => {} }
+                required
+            />
+            <TextField
+                select
+                label="Unidade"
+                name="unity"
+                value={''}
+                onChange={ () => {} }
+                required
+            >
+                { 
+                    unitys.map(option => (
+                        <MenuItem key={option} value={option}>{option}</MenuItem>
+                    ))
+                }
+            </TextField>
+            <TextField
+                label="Preço"
+                name="price"
+                value={''}
+                onChange={ () => {} }
+                InputProps={{
+                    startAdornment: <InputAdornment position="start">R$</InputAdornment>
+                }}
+            />
+        </form>
+
+    </div>
 )
 
 export default CreateList;
