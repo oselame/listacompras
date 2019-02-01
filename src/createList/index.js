5eec49486080f40a2152dd9ad76c3ad2876f97f9
Adriano Oselame <adrianooselame@gmail.com>
2019-01-31 23:38:11 -0200

    Estilizando o form

--- src/createList/index.js
+++ src/createList/index.js
@@ -3,57 +3,68 @@ import React from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 import { InputAdornment } from '@material-ui/core';
+import Button  from '@material-ui/core/Button';
+
+import './List.css';
 
 const unitys = ['kg', 'lt', 'un'];
 
+
 const CreateList = () => (
     <div className="page-container">
-
         <form className="form-container">
-            <TextField
-                label="Lista"
-                name="list"
-                value={''}
-                onChange={ () => {} }
-                required
-            />
-            <TextField
-                label="Produto"
-                name="product"
-                value={''}
-                onChange={ () => {} }
-                required
-            />
-            <TextField
-                label="Quantidade"
-                name="quantity"
-                value={''}
-                onChange={ () => {} }
-                required
-            />
-            <TextField
-                select
-                label="Unidade"
-                name="unity"
-                value={''}
-                onChange={ () => {} }
-                required
-            >
-                { 
-                    unitys.map(option => (
-                        <MenuItem key={option} value={option}>{option}</MenuItem>
-                    ))
-                }
-            </TextField>
-            <TextField
-                label="Preço"
-                name="price"
-                value={''}
-                onChange={ () => {} }
-                InputProps={{
-                    startAdornment: <InputAdornment position="start">R$</InputAdornment>
-                }}
-            />
+            <div className="form-row">
+                <TextField
+                    label="Lista"
+                    name="list"
+                    value={''}
+                    onChange={ () => {} }
+                    required
+                />
+
+                <Button variant="outlined" color="secondary">Adicionar</Button>
+            </div>
+            <div className="form-row">
+                <TextField
+                    label="Produto"
+                    name="product"
+                    value={''}
+                    onChange={ () => {} }
+                    required
+                />
+                <TextField
+                    label="Quantidade"
+                    name="quantity"
+                    value={''}
+                    onChange={ () => {} }
+                    required
+                />
+                <TextField
+                    select
+                    label="Unidade"
+                    name="unity"
+                    value={''}
+                    onChange={ () => {} }
+                    required
+                >
+                    { 
+                        unitys.map(option => (
+                            <MenuItem key={option} value={option}>{option}</MenuItem>
+                        ))
+                    }
+                </TextField>
+                <TextField
+                    label="Preço"
+                    name="price"
+                    value={''}
+                    onChange={ () => {} }
+                    InputProps={{
+                        startAdornment: <InputAdornment position="start">R$</InputAdornment>
+                    }}
+                />            
+            </div>
+
+
         </form>
 
     </div>
