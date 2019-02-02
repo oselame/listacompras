062ceac83ece5faf40735a0dc944fbeaa7ddd082
Adriano Oselame <adrianooselame@gmail.com>
2019-02-02 20:35:27 -0200

    Conectando o componente a store

--- src/createList/index.js
+++ src/createList/index.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import Form from './Form';
 import ListItem from './ListItem';
+import { Creators as ListActions } from '../store/actions/list';
 
 import './List.css';
 
-const CreateList = () => (
-    <div className="page-container">
+class CreateList extends Component {
 
-        <Form />
+    addProduct = (product, list) => {
+        this.props.addProduct(product, list);
+    };
 
-        <div className="list-items-container">
-            <ListItem />
-        </div>
+    render() {
+        return (
+            <div className="page-container">
+        
+                <Form addProduct={ this.addProduct } />
+        
+                <div className="list-items-container">
+                    <ListItem />
+                </div>
+        
+            </div>
+        );
+    }
+}
 
-    </div>
-)
+const mapStateToProps = state => ({
+    list: state.list
+});
 
-export default CreateList;
+const mapDispatchToProps = dispatch => bindActionCreators(ListActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateList);
--- src/store/index.js
+++ src/store/index.js
@@ -8,5 +8,13 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
  
+/*
+Exemplo 
+console.log('before dispatch', store.getState());
+
+store.dispatch({ type: 'ADD_PRODUCT', list: 'mes', product: { name: 'Café' } });
+
+console.log('after dispatch', store.getState());
+*/
 
 export default store;
