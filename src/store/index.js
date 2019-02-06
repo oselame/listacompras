1df07220042c25e76bc015b6fb6251a213ff96ed
Adriano Oselame <adrianooselame@gmail.com>
2019-02-06 21:45:28 -0200

    Configurando o middleware SAGA

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,35 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import reducers from './reducers';
+import sagas from './sagas';
+
 import { loadState, saveState  } from './localStorage';
 
 const localStorageState = loadState();
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = { sagaMiddleware };
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     reducers, 
     localStorageState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+        applyMiddleware(...middlewares)
+    )
 );
+
+sagaMiddleware.run(sagas);
 
 store.subscribe(() => {
     saveState({ list: store.getState().list });
 });
  
 /*
+
+https://github.com/zalmoxisus/redux-devtools-extension
+
 Exemplo 
 console.log('before dispatch', store.getState());
 
