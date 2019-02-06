10506707c585408e6c37ad454d4a93e906540a50
Adriano Oselame <adrianooselame@gmail.com>
2019-02-06 21:21:20 -0200

    Criando api com axios

---
+++ src/services/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const services = {
+    getImages: (product) => {
+        const params = {
+            searchType: 'image',
+            lr: 'lang_pt',
+            num: 1,
+            key: 'AIzaSyDQuKLfIN84QJSRbEXxb7pYqIlcWTznzMM',
+            cx: '012648785220306455351:hsaxt5uohgy',
+            q: product,
+        };
+        
+        return axios.get('https://www.googleapis.com/customsearch/v1?', params)
+            .then( resp => resp.data.items[0].link )
+            .catch( err => err );
+
+    }
+}
